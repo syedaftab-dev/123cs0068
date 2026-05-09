bf73c422a6dcbdcf46ecc10330be076e46a8cddb
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-05-09 11:28:37 +0530

    logging middleware completed

---
+++ logging_middleware/logger.js
@@ -0,0 +1,35 @@
+const axios=require('axios')
+require('dotenv').config();
+const BASE_URL=process.env.BASE_URL;
+const TOKEN=process.env.TOKEN;
+
+if(!TOKEN){
+        console.log("Token is missing, please check it once")
+}
+
+const Log=async(stack,level,package_name,message)=>{
+    
+  
+
+    try 
+    {
+        const payload={stack: stack.toLowerCase(),
+            level :level.toLowerCase(),
+            package:package_name.toLowerCase(),
+            message:message.toString()
+        };
+        await axios.post(`${BASE_URL}/logs`,payload,{
+            headers:{
+                Authorization:`Bearer${TOKEN}`,
+                'Content-Type':'application/json'
+            }
+        });
+        console.log(level.toUpperCase());
+        console.log(package_name)
+        console.log(message);
+    }catch(e){
+        console.error("Error in Logger.js file",e)
+    }
+};
+
+module.exports=Log;
